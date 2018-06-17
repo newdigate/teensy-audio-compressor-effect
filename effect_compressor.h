90fa8fb542186db50c0ddbaf26f18489bccdc323
Nic Newdigate <nicnewdigate@gmail.com>
2018-06-17 20:43:20 +0100

    Update effect_compressor.h

--- effect_compressor.h
+++ effect_compressor.h
@@ -8,8 +8,11 @@ class AudioEffectCompressor : public AudioStream {
 public:
     AudioEffectCompressor() : AudioStream(1, inputQueueArray) { }
     virtual void update(void);
+    void setThreshold(uint16_t threshold);
+    void setGainReduction(uint8_t gainReduction);
 private:
-    int16_t _threshold = 0x08FF;
+    uint16_t _threshold = 0x5000;
+    uint8_t _gainReduction = 3;
     audio_block_t *inputQueueArray[1];
 };
 
