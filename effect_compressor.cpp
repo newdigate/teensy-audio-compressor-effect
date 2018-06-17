306bc9f3460aec66e46a8af2c154b544958a367f
Nic Newdigate <nicnewdigate@gmail.com>
2018-06-17 20:44:12 +0100

    Update effect_compressor.cpp

--- effect_compressor.cpp
+++ effect_compressor.cpp
@@ -1,4 +1,3 @@
-
 #include "effect_compressor.h"
 
 void AudioEffectCompressor::update(void)
@@ -17,13 +16,21 @@ void AudioEffectCompressor::update(void)
         a1 = pa[n];
         
         if (a1 > _threshold) {
-         a1 = _threshold + (a1 - _threshold) / 3;
+            a1 = _threshold + (a1 - _threshold) / _gainReduction;
         } else if (a1 < -_threshold) {
-         a1 = -_threshold - ( - _threshold - a1) / 3;
+            a1 = -_threshold - ( - _threshold - a1) / _gainReduction;
         }
         
         pa[n] = a1;
     }
-    transmit(blocka, 0);
+    transmit(blocka);
     release(blocka);
+}
+
+void AudioEffectCompressor::setThreshold(uint16_t threshold) {
+    _threshold = threshold;
+}
+
+void AudioEffectCompressor::setGainReduction(uint8_t gainReduction) {
+    _gainReduction = gainReduction;
 }
