870e29711248e2823f231319ae712cab3f584e9e
Nic Newdigate <nicnewdigate@gmail.com>
2018-06-17 23:08:36 +0100

    Update effect_compressor.h

--- effect_compressor.h
+++ effect_compressor.h
@@ -1,5 +1,5 @@
-#ifndef PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
-#define PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
+#ifndef EFFECT_COMPRESSOR_H
+#define EFFECT_COMPRESSOR_H
 
 #include <Arduino.h>
 #include "AudioStream.h"
@@ -8,12 +8,24 @@ class AudioEffectCompressor : public AudioStream {
 public:
     AudioEffectCompressor() : AudioStream(1, inputQueueArray) { }
     virtual void update(void);
+
+    uint16_t getThreshold();
+    uint8_t getGainReduction();
+    float getInputGain();
+    bool getEnabled();
+
     void setThreshold(uint16_t threshold);
     void setGainReduction(uint8_t gainReduction);
+    void setInputGain(float inputGain);
+    void setEnabled(bool enabled);
+    void toggleEnabled();
+
 private:
+    bool _enabled = true;
     uint16_t _threshold = 0x5000;
     uint8_t _gainReduction = 3;
+    float _inputGain = 1.0;
     audio_block_t *inputQueueArray[1];
 };
 
-#endif //PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
+#endif //EFFECT_COMPRESSOR_H
